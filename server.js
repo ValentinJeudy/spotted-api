cfed8b6368039d5a4fca71c9935b6eec5834b7a5
ValentinJeudy <valentin.jeudy@gmail.com>
2018-06-15 22:10:13 +0200

    Setting a basic server express

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
-const expres = require('express')
+const express = require('express')
 const server = express()
 const Mongoose = require('mongoose')
+const config = require('config')
+const spotModel = require('./mongoose/spot.js')
+const bdd = config.get('options.BDD_URL')
+const routes = require('./routes')
+const port = config.get('options.SERVER_PORT')
+
+server.listen(config.get('options.SERVER_PORT'))
+
+Mongoose.connect(bdd)
+
+routes(server)
