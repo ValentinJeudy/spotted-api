ae419b23e5ec7a3c0eaa591b93231bb96c8d5a91
ValentinJeudy <valentin.jeudy@gmail.com>
2018-06-17 22:44:29 +0200

    getVisibleSpots request is working

--- controllers/index.js
+++ controllers/index.js
@@ -11,29 +11,19 @@ module.exports = {
   },
   getVisibleSpots: (req, res, northEastCorner, southWestCorner) => {
 
-    // const neCorner = JSON.parse(northEastCorner)
     const nec = JSON.parse(northEastCorner)
     const swc = JSON.parse(southWestCorner)
 
-    console.log('nec ===> ', require('util').inspect(nec.lat, { colors: true, depth: 2 }))
-    console.log('swc ===> ', require('util').inspect(swc.lat, { colors: true, depth: 2 }))
+    console.log('lat ===> ', require('util').inspect(nec.lat - swc.lat, { colors: true, depth: 2 }))
+    console.log('lng ===> ', require('util').inspect(nec.lng - swc.lng, { colors: true, depth: 2 }))
 
     spotModel.find({
-      localisation:{
-        $elemMatch: {
-          lat: { $gte: swc.lat, $lte: nec.lat },
-          lng: { $gte: nec.lng, $lte: swc.lng }
-        }
-      }
-      // {
-      //   lat: { $gte: swc.lat, $lte: nec.lat },
-      //   lng: { $gte: nec.lng, $lte: swc.lng }
-      // }
+      "localisation.lat": { $gte: swc.lat, $lte: nec.lat },
+      "localisation.lng": { $gte: swc.lng, $lte: nec.lng },
     }).exec((err, data) => {
       if (err) {
         res.status(500).send('Something broke : ', err)
       }
-      console.log('data ===> ', data)
       res.json(data)
     })
   }
