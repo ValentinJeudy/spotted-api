5f6428a811439e2a36f602cf87885305bcc6bc6b
ValentinJeudy <valentin.jeudy@gmail.com>
2018-09-16 21:50:45 +0200

    adding socket connection with socket.io

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express')
-const server = express()
+const app = express()
 const Mongoose = require('mongoose')
 const config = require('config')
 const bdd = config.get('options.BDD_URL')
-const routes = require('./routes')
+// const routes = require('./routes')
 const port = config.get('options.SERVER_PORT')
-// const socket = require('socket.io')
+const socket = require('socket.io')
 
-server.listen(config.get('options.SERVER_PORT'))
+const server = app.listen(config.get('options.SERVER_PORT'))
 
 console.info('listening on port :', port)
 
 Mongoose.connect(bdd)
 
-routes(server)
+app.use(express.static('public'))
+
+const io = socket(server)
+
+io.on('connection', socket => {
+  console.log('===> Connection to socket as been made <====')
+})
+
+// routes(server)
